783f3c9394fb68cb742e2f04d2bfe7be9552e67d
Eyyub38 <erenbaktir@gmail.com>
2025-06-12 04:25:55 +0300

    Move SFXs

---
+++ Assets/Scripts/Gameplay/Ledge.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ledge : MonoBehaviour{
+    [SerializeField] int xDir;
+    [SerializeField] int yDir;
+}
